41683ec2c69cf7833d414c179c398654df69fb9f
Mazayw <mazayw@gmail.com>
2022-05-24 01:38:29 +0300

    feat: add fetch sneakers from mockApi

--- src/App.js
+++ src/App.js
@@ -3,37 +3,21 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
-const arr = [
-  {
-    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: 12999,
-    imageUrl: '/img/sneakers/1.jpg',
-  },
-  {
-    title: 'Мужские Кроссовки Nike Air Max 270',
-    price: 12999,
-    imageUrl: '/img/sneakers/2.jpg',
-  },
-  {
-    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: 8499,
-    imageUrl: '/img/sneakers/3.jpg',
-  },
-  {
-    title: 'Кроссовки Puma X Aka Boku Future Rider',
-    price: 12999,
-    imageUrl: '/img/sneakers/4.jpg',
-  },
-];
-
 function App() {
+  const [items, setItems] = React.useState([]);
   const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch('https://628c04563df57e983ec85bcd.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => setItems(data));
+  }, []);
 
   return (
     <div className="wrapper clear">
-      {cartOpened ? (
-        <Drawer onClickCart={() => setCartOpened(!cartOpened)} />
-      ) : null}
+      {cartOpened && <Drawer onClickCart={() => setCartOpened(!cartOpened)} />}
       <Header onClickCart={() => setCartOpened(!cartOpened)} />
       <div className="content p-40">
         <div className="d-flex align-center mb-40 justify-between">
@@ -44,7 +28,7 @@ function App() {
           </div>
         </div>
         <div className="d-flex flex-wrap">
-          {arr.map((obj) => (
+          {items.map((obj) => (
             <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
           ))}
         </div>
